fb8489dc3733aa9e9488bcaca0ea1b781e54f422
Tajetti <talhettialvaro@edu.unifil.br>
2025-05-20 19:26:22 -0300

    Implementando métodos para contar os nós da Árvore

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,3 +1,12 @@
 public class Arvore {
     No raiz;
+
+    //Contando os nós da árvore
+    public int contarNos(No no) {
+        if (no == null) {
+            return 0;
+        }
+        return 1 + contarNos(no.esquerdo) + contarNos(no.direito);
+    }
+
 }
