2aecf903121f1adc9bb0fe0a0a99699b6cc36f20
Tajetti <talhettialvaro@edu.unifil.br>
2025-05-20 19:41:08 -0300

    Implementando o método contador

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,3 +1,17 @@
 public class Arvore {
+    // Implementando a Arvore
+    No raiz;
+
+    public Arvore() {
+        this.raiz = null;
+    }
+
+    /*Implementando contador int*/
+    private int contarNos(No no) {
+        if (no == null) {
+            return 0;
+        }
+        return 1 + contarNos(no.esquerdo) + contarNos(no.direito);
+    }
 
 }
