dda3dd7bdd44d8de5b5736a73a6785c3367fff22
Tajetti <talhettialvaro@edu.unifil.br>
2025-06-07 16:53:07 -0300

    Adição da rotação simples direita e esquerda

--- src/ArvoreAVL.java
+++ src/ArvoreAVL.java
@@ -14,5 +14,29 @@ public class ArvoreAVL {
         return alturaAVL(no.esquerda) - alturaAVL(no.direita);
     }
 
+    NoAVL RotacaoDireita(NoAVL y){
+        NoAVL x = y.esquerda;
+        NoAVL T2 = x.direita;
 
+        x.direita = y;
+        y.esquerda = T2;
+
+        y.altura = Math.max(alturaAVL(y), alturaAVL(x)) + 1;
+        x.altura = Math.max(alturaAVL(y), alturaAVL(x)) + 1;
+
+        return x;
+    }
+
+    NoAVL RotacaoEsquerda(NoAVL x){
+        NoAVL y = x.direita;
+        NoAVL T2 = y.esquerda;
+
+        y.esquerda = x;
+        x.direita = T2;
+
+        x.altura = Math.max(alturaAVL(y), alturaAVL(x)) + 1;
+        y.altura = Math.max(alturaAVL(y), alturaAVL(x)) + 1;
+
+        return y;
+    }
 }
